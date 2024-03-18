db1874d71f21597e8065baa99fbf054652a0875b
junseong kim <anima94@kakao.com>
2024-03-18 21:34:39 +0900

    feat : Mongo Connection

--- repository/mongo/root.go
+++ repository/mongo/root.go
@@ -1,16 +1,36 @@
 package mongo
 
 import (
+	"context"
 	"go-ecommerce/config"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Mongo struct {
 	config *config.Config
+
+	client *mongo.Client
+	db     *mongo.Database
 }
 
 func NewMongo(config *config.Config) (*Mongo, error) {
 	m := &Mongo{
 		config: config,
 	}
+
+	ctx := context.Background()
+	var err error
+
+	if m.client, err = mongo.Connect(ctx, options.Client().ApplyURI(config.Mongo.URI)); err != nil {
+		panic(err)
+	} else if err = m.client.Ping(ctx, nil); err != nil {
+		panic(err)
+	} else {
+		m.db = m.client.Database(config.Mongo.Db)
+		// TODO 컬렉션 연결
+	}
+
 	return m, nil
 }
