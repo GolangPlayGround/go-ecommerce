f468a40b1d7847c347869e324f512f74f942021c
junseong kim <anima94@kakao.com>
2024-03-19 09:22:34 +0900

    feat: add mongo index function

--- repository/mongo/root.go
+++ repository/mongo/root.go
@@ -2,8 +2,10 @@ package mongo
 
 import (
 	"context"
+	"fmt"
 	"go-ecommerce/config"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,7 +32,42 @@ func NewMongo(config *config.Config) (*Mongo, error) {
 	} else {
 		m.db = m.client.Database(config.Mongo.Db)
 		// TODO 컬렉션 연결
+
+		//createIndex(m.db.Collection("test"), []string{"key"},[]string{})
 	}
 
 	return m, nil
+}
+
+func createIndex(collection *mongo.Collection, indexes, uniques []string) error {
+	opt := options.Index()
+
+	for _, k := range indexes {
+		model := mongo.IndexModel{
+			Keys:    bson.D{{Key: k, Value: 1}},
+			Options: opt,
+		}
+
+		if res, err := collection.Indexes().CreateOne(context.Background(), model); err != nil {
+			panic(err)
+		} else {
+			fmt.Println(res)
+		}
+	}
+
+	opt.SetUnique(true)
+
+	for _, k := range uniques {
+		model := mongo.IndexModel{
+			Keys:    bson.D{{Key: k, Value: 1}},
+			Options: opt,
+		}
+
+		if res, err := collection.Indexes().CreateOne(context.Background(), model); err != nil {
+			panic(err)
+		} else {
+			fmt.Println(res)
+		}
+	}
+	return nil
 }
